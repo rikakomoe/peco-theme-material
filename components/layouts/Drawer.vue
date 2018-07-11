48301ae02e55e6f9dee9823e3fddca5e34deef0b
LI JIAHAO <lijiahao99131@gmail.com>
2018-07-11 23:10:54 +0800

    :sparkles: drawer
    Signed-off-by: LI JIAHAO <lijiahao99131@gmail.com>

--- components/layouts/Drawer.vue
+++ components/layouts/Drawer.vue
@@ -4,14 +4,45 @@
       <md-icon>menu</md-icon>
     </md-button>
     <md-drawer :md-active.sync="showNavigation">
-      <md-toolbar class="md-transparent" md-elevation="0">
-        <span class="md-title">My App name</span>
-      </md-toolbar>
+      <div class="sidebar-header" :style="`background-image: url('https://ekyu.moe/res/img/header.jpg')`">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <div class="toolbar-top" />
+          <div class="toolbar-top" />
+          <span class="md-title"><md-avatar class="md-avatar-icon">A</md-avatar></span>
+          <md-button class="md-icon-button burger-close">
+            <md-icon>clear_all</md-icon>
+          </md-button>
+          <div class="toolbar-top" />
+        </md-toolbar>
+        <md-list>
+          <md-list-item md-expand>
+            <md-icon>whatshot</md-icon>
+            <span class="md-list-item-text">News</span>
+
+            <md-list slot="md-expand">
+              <md-list-item class="md-inset">World</md-list-item>
+              <md-list-item class="md-inset">Europe</md-list-item>
+              <md-list-item class="md-inset">South America</md-list-item>
+            </md-list>
+          </md-list-item>
+        </md-list>
+      </div>
 
       <md-list>
-        <md-list-item>
-          <md-icon>move_to_inbox</md-icon>
-          <span class="md-list-item-text">Inbox</span>
+        <md-list-item to="/">
+          <md-icon>home</md-icon>
+          <span class="md-list-item-text">主页</span>
+        </md-list-item>
+
+        <md-list-item md-expand>
+          <md-icon>whatshot</md-icon>
+          <span class="md-list-item-text">News</span>
+
+          <md-list slot="md-expand">
+            <md-list-item class="md-inset">World</md-list-item>
+            <md-list-item class="md-inset">Europe</md-list-item>
+            <md-list-item class="md-inset">South America</md-list-item>
+          </md-list>
         </md-list-item>
 
         <md-list-item>
@@ -27,6 +58,13 @@
         <md-list-item>
           <md-icon>error</md-icon>
           <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+
+        <md-divider />
+
+        <md-list-item href="https://github.com/rikakomoe/peco-theme-material" target="_blank">
+          <span class="md-list-item-text">主题 - Material</span>
+          <md-badge md-content="i" />
         </md-list-item>
       </md-list>
     </md-drawer>
@@ -55,9 +93,45 @@
     z-index: 8;
   }
 
+  .md-avatar {
+    width: 54px;
+    height: 54px;
+  }
+
   .md-drawer {
     position: fixed;
     height: 100%;
     width: 280px;
+  }
+
+  .badge-count {
+    border-radius: 0;
+  }
+
+  .md-divider {
+    margin: 0.5rem 0 !important;
+  }
+
+  .toolbar-top {
+    width: 100%;
+    height: 20px;
+  }
+
+  .sidebar-header {
+    background-repeat: no-repeat;
+    background-position: top center;
+    background-size: 100%;
+  }
+
+  .sidebar-header .md-list {
+    background: transparent;
+  }
+
+  .md-toolbar {
+    justify-content: space-between;
+  }
+
+  .md-list.md-theme-default .md-list-item-container:not(.md-list-item-default):not([disabled]):hover {
+    background: inherit;
   }
 </style>
